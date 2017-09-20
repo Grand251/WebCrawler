5c9cdfe74d02b0a4d4561fd0ebdb97dab5f9befb
TaukSnarkyAgrud <andrew.t.aldridge@gmail.com>
2017-09-20 10:51:58 -0500

    Refining and completing
    Added notes

--- WebCrawler.java
+++ WebCrawler.java
@@ -31,7 +31,7 @@ public class WebCrawler {
     treeFromSeed mainTree = new treeFromSeed;
     
     private static void crawl(String URL, int depth) {
-        if (depth <=0 && !mainTree.contains(URL)){
+        if (depth <=0 && !mainTree.contains(URL)){     // Check that the base recursive case is present and add leaves to tree with no metadata
             mainTree.intake(URL);
         }
         if (depth > 0) {
@@ -42,7 +42,7 @@ public class WebCrawler {
             String link;
             for (int i = 0; i < links.length; i++) {
                 link = links[i];
-                if (!treeFromSeed.Contains(URL)){
+                if (!treeFromSeed.Contains(URL)){       // if the tree already holds this URL somewhere, Don't expand it. 
                     crawl(link, --depth);
                 }
             }
@@ -57,7 +57,6 @@ public class WebCrawler {
     //Creates and stores UNI-Gram
     private static char[] processPage(String HTML) {
         char a_char;
-        
         int indexOf;
         int[] freqOfChar95 = new int[95];
         for (int pos = 0; pos < str.length(); ++pos){
@@ -67,27 +66,13 @@ public class WebCrawler {
                 freqOfChar[indexOf]++;
             }
         }
-        //DO WE RETURN THE ARRAY OR MAKE A GLOBAL CLASS?? AUTO SAVE
         return freqOfChar[];
     }
-    //Returns String array of all links found in HTML
-    private static String[] getLinks(String HTML) {
-        String[] links;
-        Pattern p = Pattern.compile("href=\"(.*?)\"");
-        Matcher m = p.matcher(HTML);
-        String url = null;
-        magnitude = 0;
-        while (m.find()) {
-            links[magnitude] = m.group(1);
-            magnitude++;
-        }
-        return links;
-    }
     
-    //returns the index of the same char in the 95 chars we are looking at
+     //returns the index of the same char in the 95 chars we are looking at
     private static int whichChar(char theChar){
         int theIndex = -1;
-        // Create table for comparison Loop over all possible ASCII codes to max.
+        // Create table for comparison Loop over all possible ASCII codes up to max.
         int min = 0;
         int max =95;
         char[] compCharArr= new char[95];
@@ -103,54 +88,78 @@ public class WebCrawler {
         return theIndex;
     }
     
+    //Returns String array of all links found in HTML
+    private static String[] getLinks(String HTML) {
+        String[] links;
+        Pattern p = Pattern.compile("href=\"(.*?)\"");
+        Matcher m = p.matcher(HTML);
+        magnitude = 0;
+        while (m.find()) {
+            links[magnitude] = m.group(1);
+            magnitude++;
+        }
+        return links;
+    }
 }
 
 public class treeFromSeed {
     
     TreeNode aTN = new TreeNode;
-    hash aHash = new hash;
+    //hash aHash = new hash;
+    //Hashing is unnecessary since URL's are already unique.
+    //Store in array and search array. 
+    list<string> haveURL;
+    
   
     public static void intake(string URL){
+        if (!haveURL.contians(URL)){
+            haveURL.add(0,URL);            
+        }
         //add url to the tree; overload the paramerters so if HTML is given, add HTML and LINKS to tree element
         //also load all links into a hash to test for repeating links. test first.
     }
     public static void intake(string URL, string HTML, char[] unigraham){
+        if (!haveURL.contians(URL)){
+            haveURL.add(0,URL);            
+        }
         //add url to the tree; overload the paramerters so if HTML is given, add HTML and LINKS to tree element
         //also load all links into a hash to test for repeating links. test first.
     }
     public static bool Contains(URL){
-        //return true if aHash includes URL
+        if (!haveURL.contians(URL)){
+            return True;            
+        }
+        return False;
+    }
+    
+    public static bool printTree(){
         
     }
+    public static bool saveTreeToFile(){
+        
+    }
+    
 }
 
 public class TreeNode {
 
     string aURL;
     string HTML;
-    char[] aUnigraham;
+    char[] aUnigram;
     
     TreeNode parent;
     List<TreeNode> children;
 
     public TreeNode() {
-        this.data = data;
+        this.aURL = URL;
+        this.HTML = HTML;
+        this.aUnigram = aUnigram;
         this.children = new LinkedList<TreeNode>();
     }
-
     public TreeNode add(T child) {
-        TreeNode childNode = new TreeNode(child);
+        TreeNode childNode = new TreeNode();
         childNode.parent = this;
-        this.children.add(childNode);
+        this.children.add();
         return childNode;
     }
-    public TreeNode addChild(T child) {
-        TreeNode childNode = new TreeNode(child);
-        childNode.parent = this;
-        this.children.add(childNode);
-        return childNode;
-    }
-
-    // other features ...
-
 }
