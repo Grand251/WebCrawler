b71c2a41305f54ada9229941951b384daf43631e
Grand251 <austinrm66@gmail.com>
2017-09-20 18:28:35 -0500

    Update WebCrawler.java

--- WebCrawler.java
+++ WebCrawler.java
@@ -3,14 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 package webcrawler;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.File;
 import java.io.FileWriter;
+import java.net.URL;
+import java.net.*;
+import java.io.DataInputStream;
+import java.io.IOException;
 
 /**
  *
@@ -22,93 +25,92 @@ public class WebCrawler {
         int pageNum = 0;
         int maxDepth;
         String startURL;
-        Scanner scanner = new Scanner(System. in); 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Starting URL: ");
         startURL = scanner.nextLine();
         System.out.println("Depth: ");
         maxDepth = Integer.parseInt(scanner.nextLine());
-       
-        for (int depth = 1; depth <= maxDepth; depth++)
+
+        for (int depth = 1; depth <= maxDepth; depth++) {
             pageNum = crawl(startURL, depth, pageNum);
-        
-    }
-    
-
-    
-
-    private static int crawl(String URL, int depth, int pageNum) {
-        pageNum++;
-        String HTML = getPage(URL); // Get HTML code
-        processPage(HTML, pageNum); //Create and store the Unigram    
-        
-        if (depth > 0) {
-            String[] links = getLinks(HTML);
-            String link;
-            for (int i = 0; i < links.length; i++) {
-                link = links[i];
-                crawl(link, --depth, pageNum);
-            }
         }
+
+    }
+
+    public static int crawl(String URL, int depth, int pageNum) {
+
+        try {
+            String HTML = getPage(URL); // Get HTML code. Throws Exception if URL not valid
+
+            pageNum++; //Increment pageNum if exception not thrown
+            processPage(HTML, pageNum); //Create Unigram and Store HTML
+
+            if (depth > 0) {
+                String[] links = getLinks(HTML);
+                String link;
+                for (int i = 0; i < links.length; i++) {
+                    link = links[i];
+                    crawl(link, --depth, pageNum);
+                }
+            }
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL.");
+        } catch (IOException e) {
+            System.out.println("HTML could not be retrieved.");
+        }
+
         return pageNum;
     }
-    //Returns HTML string of page
-       private static String getPage(String URL_link) throws MalformedURLException, IOException {
-                String HTML = "";
-            java.net.URL url = new URL(URL_link); //args[0]
-            URLConnection connection = url.openConnection();
 
-            try {
-                  DataInputStream in = new DataInputStream(connection.getInputStream());
-                    String text;
-                    while ((text = in.readLine()) != null)
-                    {
-                       HTML += "  " + text + "\n");
-                    }
-                }
-            catch (IOException e) {
-                        System.out.println("Exception Occurred:");
-                        e.printStackTrace();
-                    }
+    //Returns HTML string of page
+    public static String getPage(String URL_link) throws MalformedURLException, IOException {
+        String HTML = "";
+
+        java.net.URL url = new URL(URL_link); //args[0]
+        URLConnection connection = url.openConnection();
+        DataInputStream in = new DataInputStream(connection.getInputStream());
+        String text;
+        while ((text = in.readLine()) != null) {
+            HTML += "  " + text + "\n";
+        }
 
         return HTML;
     }
-    
+
     //Creates UNI-Gram and store page
-    private static void processPage(String HTML, int pageNum) {
+    public static void processPage(String HTML, int pageNum) {
         char myChar;
         int indexOf;
-        
+
         int[] uniGram = new int[95];
-        for (int pos = 0; pos < HTML.length(); ++pos){
+        for (int pos = 0; pos < HTML.length(); ++pos) {
             myChar = HTML.charAt(pos);
             indexOf = (int) myChar;
             if (indexOf >= 32 && indexOf < 127) {
                 uniGram[indexOf - 32]++;
             }
         }
-       
+
         try {
             File file = new File("./pages/" + pageNum + ".txt");
             FileWriter writer = new FileWriter(file);
             writer.write(HTML);
             writer.flush();
             writer.close();
-        } catch (Exception e) {
-            
+        } catch (IOException e) {
+            System.out.println("File could not be written.");
         }
-        
-        
+
     }
-    
-    
+
     //Returns String array of all links found in HTML
-    private static String[] getLinks(String HTML) {
-        String[] links;
+    public static String[] getLinks(String HTML) {
+        String[] links = {"href=\"auburn.edu\""};
         Pattern p = Pattern.compile("href=\"([^\"]*)\"");
         Matcher m = p.matcher(HTML);
-        int  magnitude = 0;
+        int magnitude = 0;
         while (m.find()) {
-            m.group(1)
+            m.group(1);
             links[magnitude] = m.group(1);
             magnitude++;
         }
